9a110ca5ab28d1bb53b59471b7ef881b4329ead9
Robert Petit <robbie.petit@gmail.com>
2014-10-13 00:49:55 -0400

    Not limited to yesterday, user can specify date

---
+++ fitbit-steps.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+'''
+    fitbit-steps.py
+    
+    Reads credentials from secret module then queries FitBit for total step 
+    count. That's it!
+'''
+import argparse as ap
+from datetime import date, datetime
+
+import fitbit
+
+from secret import *
+
+if __name__ == '__main__':
+    parser = ap.ArgumentParser(prog='fitbit-steps.py', 
+                               conflict_handler='resolve', 
+                               description="Get total steps for a given date")
+
+    parser.add_argument('-d', '--date', default=date.today(),
+                        metavar="STR", type=str,
+                        help='A date formated like YYYY-MM-DD (Default: Today)')
+    parser.add_argument('-n','--name', metavar="STR", type=str, required=True,
+                        default='robert', help='Person to pull steps for.')
+    parser.add_argument('-h', '--help', action='help', 
+                        help='Show this help message and exit')
+    
+    args = parser.parse_args()
+
+    auth_client = fitbit.Fitbit(
+        CONSUMER_KEY, 
+        CONSUMER_SECRET_KEY,
+        resource_owner_key=USER_KEY[args.name],
+        resource_owner_secret=USER_SECRET_KEY[args.name]
+    )
+    
+    if args.date != date.today():
+        if args.date == 'yesterday':
+            args.date = date.today() - timedelta(1)
+        else:
+            args.date = datetime.strptime(args.date, "%Y-%m-%d")
+
+    stats = auth_client.activities(date=args.date)
+
+    print stats['summary']['steps']
